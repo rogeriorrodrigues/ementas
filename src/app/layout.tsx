0dda76e751b549f4f2237c5ce711c62877a59f77
Rogério Rodrigues <rogeriorodrigues@outlook.com>
2025-04-30 15:17:59 -0300

    ajuste de path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,12 @@ import "./globals.css";
 const inter = localFont({
   src: [
     {
-      path: "/fonts/Inter-Regular.woff2",
+      path: "../../public/fonts/Inter-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "/fonts/Inter-Bold.woff2",
+      path: "../../public/fonts/Inter-Bold.woff2",
       weight: "700",
       style: "normal",
     },
