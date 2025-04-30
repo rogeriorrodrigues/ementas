85d1c3bb1631ea9a6e1908c6244090c939d38f6f
Rogério Rodrigues <rogeriorodrigues@outlook.com>
2025-04-30 15:13:00 -0300

    ajuste no layout tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,21 @@ import { cn } from "@/ui/lib";
 import localFont from 'next/font/local';
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = localFont({
+  src: [
+    {
+      path: "/fonts/Inter-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/fonts/Inter-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+});
 
 export const metadata = {
   title: AI_NAME,
